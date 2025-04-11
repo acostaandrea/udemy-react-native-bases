e292c4eec15babd5bb72c8d3f4d540b7a60ebc4f
Andrea Acosta <aacosta@quadiontech.com>
2025-04-11 17:27:16 -0300

    implement AuthContext and LoginPage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import './App.css'
-import { Counter } from './components/Counter'
+import "./App.css";
+import { Counter } from "./components/Counter";
 // import { BasicTypes } from './typescript/BasicTypes'
+import { LoginPage } from "./components/LoginPage";
+import { AuthProvider } from "./context/AuthContext";
 
-function App() {  
-
+function App() {
   return (
     <>
-    <div className='flex flex-col justify-center items-center h-svh'>
-      <h1 className='text-4xl mb-5'>React + TS</h1>
-      <Counter />
-    </div>
+      <AuthProvider>
+        <div className="flex flex-col justify-center items-center h-svh">
+          <h1 className="text-4xl mb-5">React + TS</h1>
+          <Counter />
+        <LoginPage />
+        </div>
+      </AuthProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
